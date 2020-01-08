f4d7f0ba7356dcb88a549f6b4dc8ca263531abbc
Wechuli <wechulipaul@yahoo.com>
2020-01-08 22:20:29 +0300

    preferring annotations over inference

--- projects/type_annotation/variableAnnotations.ts
+++ projects/type_annotation/variableAnnotations.ts
@@ -45,5 +45,26 @@ const json = `
     "y":20
 }
 `;
-const coordinates = JSON.parse(json);
+const coordinates: { x: number; y: number } = JSON.parse(json);
 console.log(coordinates);
+
+// 2) When we declare a variable on one line and initialize it later
+let words = ["red", "green", "blue"];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "green") {
+    foundWord = true;
+  }
+}
+
+// 3) When inference doesn't work
+
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false; // we can use a pipe operation to say a type can be either this or that
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] < 0) {
+    numberAboveZero = numbers[i];
+  }
+}
