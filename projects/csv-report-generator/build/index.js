56be9e0326b3c42b9a4c65d0754dd6ca2377b943
Wechuli <wechulipaul@yahoo.com>
2020-02-06 10:36:12 +0300

    need for enums

--- projects/csv-report-generator/build/index.js
+++ projects/csv-report-generator/build/index.js
@@ -1,2 +1,26 @@
 "use strict";
-console.log("Hi there");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+var matches = fs_1.default
+    .readFileSync("football.csv", { encoding: "utf-8" })
+    .split("\n")
+    .map(function (row) {
+    return row.split(",");
+});
+var homeWin = "H";
+var awayWin = "A";
+var drawMatch = "D";
+var manUnitedWins = 0;
+for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
+    var match = matches_1[_i];
+    if (match[1] === "Man United" && match[5] === homeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === "Man United" && match[5] === awayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(manUnitedWins);
