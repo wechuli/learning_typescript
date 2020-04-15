34278d1d54f2b88dfb094445b846ba80a026d794
Wechuli <wechulipaul@yahoo.com>
2020-04-15 13:12:38 +0300

    adding eventing system with async events

--- projects/webframework/src/index.ts
+++ projects/webframework/src/index.ts
@@ -1,7 +1,16 @@
 import { User } from "./models/User";
+import axios from "axios";
 
 const user = new User({ name: "Paul", age: 27 });
 
+axios.post("http://localhost:3000/users", {
+  name: "myname",
+  age: 20,
+});
+
+axios
+  .get("http://localhost:3000/users")
+  .then((result) => console.log(result.data));
 console.log(user.get("name"));
 console.log(user.get("age"));
 
@@ -9,3 +18,12 @@ user.set({ name: "newname", age: 9999 });
 user.set({ name: "Wechuli" });
 
 console.log(user);
+
+user.on("change", () => console.log("change1 was triggered"));
+user.on("change", () => console.log("change2 was triggered"));
+user.on("blur", () => console.log("blur was triggered"));
+
+console.log(user);
+
+user.trigger("change");
+user.trigger("blur");
--- projects/webframework/src/models/User.ts
+++ projects/webframework/src/models/User.ts
@@ -3,7 +3,10 @@ interface IUser {
   age?: number;
 }
 
+type Callback = () => void;
+
 export class User {
+  events: { [key: string]: Callback[] } = {};
   constructor(private data: IUser) {}
 
   get(propName: string): number | string {
@@ -12,5 +15,21 @@ export class User {
 
   set(update: IUser): void {
     Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  async trigger(eventName: string): Promise<void> {
+    const handlers = this.events[eventName];
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+    handlers.forEach((callback) => {
+      callback();
+    });
   }
 }
